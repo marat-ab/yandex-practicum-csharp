f7307ae5603a4825b0f0e7245c053325f093dada
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-04-01 17:52:44 +0500

    [add] validation into event request dto

--- EventManagementServices/Models/EventRequest.cs
+++ EventManagementServices/Models/EventRequest.cs
@@ -1,9 +1,26 @@
-﻿namespace EventManagementServices.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class EventRequest
+namespace EventManagementServices.Models;
+
+public class EventRequest : IValidatableObject
 {
-    public required string Title { get; init; }
+    [Required(ErrorMessage = "Заголовок обязателен для заполнения")]
+    public string Title { get; init; } = string.Empty;
+
     public string Description { get; init; } = string.Empty;
-    public required DateTime StartAt { get; init; }
-    public required DateTime EndAt { get; init; }
+
+    [Required(ErrorMessage = "Дата/время начала события обязателено для заполнения")]
+    public DateTime StartAt { get; init; }
+
+    [Required(ErrorMessage = "Дата/время окончания события обязателено для заполнения")]
+    public DateTime EndAt { get; init; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndAt < StartAt)
+        {
+            var result = new ValidationResult("EndAt должно быть позже StartAt.");
+            yield return result;
+        }
+    }
 }
