e4ea55a0a2e35e4e5e71b8dcbc0a025a73f5115e
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-04-13 21:41:37 +0500

    [add] custom exception handling middleware

---
+++ EventManagementService/Middlewares/CustomExceptionHandlingMiddleware.cs
@@ -0,0 +1,67 @@
+﻿using EventManagementService.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagementService.Middlewares;
+
+public class CustomExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    private readonly ILogger<CustomExceptionHandlingMiddleware> _logger;
+
+    public CustomExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<CustomExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleException(httpContext, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext httpContext, Exception ex)
+    {
+        _logger.LogError(
+            ex,
+            "Unhandled exception. Method={Method}, Path={Path}",
+            httpContext.Request.Method,
+            httpContext.Request.Path);
+
+        if (httpContext.Response.HasStarted)
+        {
+            return;
+        }
+
+        var statusCode = MapStatusCode(ex);
+
+        httpContext.Response.StatusCode = statusCode;
+        httpContext.Response.ContentType = "application/json";
+
+        var error = new ProblemDetails
+        {
+            Status = statusCode,
+            Detail = ex.Message
+        };
+
+        await httpContext.Response.WriteAsJsonAsync(error);
+    }
+
+    private static int MapStatusCode(Exception ex)
+        => ex switch
+        {
+            ValidationException _ => StatusCodes.Status400BadRequest,
+            EventNotFoundException _ => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+}
