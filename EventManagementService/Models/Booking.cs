f2bb7f990ebadb5db0292949e6644272454347cb
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-04-27 17:06:40 +0500

    [add] models booking and booking status

---
+++ EventManagementService/Models/Booking.cs
@@ -0,0 +1,8 @@
+﻿namespace EventManagementService.Models;
+
+public sealed record Booking(
+    Guid Id,
+    int EventId,
+    BookingStatus Status,
+    DateTime CreatedAt,
+    DateTime? ProcessedAt = null);
