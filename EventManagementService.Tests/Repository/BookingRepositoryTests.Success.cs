bc1048df9bfc9b25bca3b73d8064d2255c0ed6fb
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-04-28 18:17:46 +0500

    [add] blank test files for booking repository

---
+++ EventManagementService.Tests/Repository/BookingRepositoryTests.Success.cs
@@ -0,0 +1,14 @@
+﻿using EventManagementService.Exceptions;
+using EventManagementService.Models;
+using EventManagementService.Models.Extensions;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.Tests.Repository;
+
+public partial class BookingServiceTests
+{
+    
+}
---
+++ EventManagementService.Tests/Repository/BookingRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using EventManagementService.Models;
+using EventManagementService.Repository;
+using EventManagementService.Services;
+using Microsoft.Extensions.Logging;
+
+namespace EventManagementService.Tests.Repository;
+
+public partial class BookingRepositoryTests : IAsyncLifetime
+{
+    private readonly IBookingRepository _bookingRepository;
+
+    public BookingRepositoryTests()
+    {
+        _bookingRepository = new BookingRepository();
+    }
+   
+    // IAsyncLifetime
+    public async Task InitializeAsync()
+    {
+    }
+
+    public async Task DisposeAsync()
+    {
+        
+    }
+}
