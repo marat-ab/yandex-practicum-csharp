4ad30098c575bb4cf741d5f4ba432debd1537918
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-05-14 17:12:19 +0500

    [add] properties and methods into Event

--- EventManagementService/Models/Event.cs
+++ EventManagementService/Models/Event.cs
@@ -1,8 +1,29 @@
 ﻿namespace EventManagementService.Models;
 
-public sealed record Event(
-    Guid Id,
-    string Title,
-    string Description,
-    DateTime StartAt,
-    DateTime EndAt);
+public sealed class Event
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public int TotalSeats { get; set; }
+    public int AvailableSeats { get; set; }
+    public DateTime StartAt { get; set; }
+    public DateTime EndAt { get; set; }
+
+    public bool TryReserveSeats(int count = 1)
+    {
+        if (AvailableSeats - count < 0)
+            return false;
+
+        AvailableSeats -= count;
+        return true;
+    }
+
+    public void ReleaseSeats(int count = 1)
+    {
+        AvailableSeats += count;
+
+        if(AvailableSeats > TotalSeats)
+            AvailableSeats = TotalSeats;
+    }
+}
