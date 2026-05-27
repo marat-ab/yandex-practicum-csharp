77b3b7528273c5a8bc71a2b8ab3c6cce76d08b6b
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-05-27 17:54:55 +0500

    [add] reg AppDbContext in DI and create tables in db by EnsureCreated

--- EventManagementService/Program.cs
+++ EventManagementService/Program.cs
@@ -1,6 +1,8 @@
-﻿using EventManagementService.HostedServices;
+﻿using EventManagementService.DataAccess;
+using EventManagementService.HostedServices;
 using EventManagementService.Middlewares;
 using EventManagementService.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,9 +14,21 @@ builder.Services.AddScoped<IBookingService, BookingService>();
 
 builder.Services.AddHostedService<BookingHostedService>();
 
+var connectionString = builder.Configuration.GetConnectionString("Default")
+    ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<CustomExceptionHandlingMiddleware>();
