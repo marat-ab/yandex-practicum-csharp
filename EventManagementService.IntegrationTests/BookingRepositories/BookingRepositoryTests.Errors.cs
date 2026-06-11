bcccda7b17746ca9d366a1d9a4dd73eb2316c06c
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-06-11 17:50:01 +0500

    [add] booking repository tests

---
+++ EventManagementService.IntegrationTests/BookingRepositories/BookingRepositoryTests.Errors.cs
@@ -0,0 +1,34 @@
+﻿using EventManagementService.Exceptions;
+using EventManagementService.Models;
+using EventManagementService.Repositories;
+using EventManagementService.Services;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.IntegrationTests.BookingRepositories;
+
+public partial class BookingRepositoryTests
+{
+    // Получение брони по несуществующему id
+    [Fact]
+    [Trait("Category", "Success")]
+    public async Task GetBookingWithNotExistingId()
+    {
+        await ResetDatabaseAsync();
+
+        // Arrange
+        await using var context = CreateContext();
+        var bookingId = Guid.NewGuid();
+
+        // Act
+        var repository = new BookingRepository(context);
+        Func<Task> act = async () => await repository.SelectBookingByIdAsync(bookingId);
+
+        // Assert
+        await act.Should().ThrowAsync<BookingNotFoundException>()
+           .WithMessage($"Can't get booking with id = {bookingId}. It is absent");
+    }
+}
