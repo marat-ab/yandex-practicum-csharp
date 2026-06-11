bfec3d56305f83755d165d76434bc99e03107b47
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-06-11 17:49:41 +0500

    [add] event repository errors tests

---
+++ EventManagementService.IntegrationTests/EventRepositories/EventRepositoryTests.Errors.cs
@@ -0,0 +1,59 @@
+﻿using EventManagementService.Exceptions;
+using EventManagementService.Models;
+using EventManagementService.Repositories;
+using EventManagementService.Services;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.IntegrationTests.EventRepositories;
+
+public partial class EventRepositoryTests
+{
+    // Попытка получить событие с несуществующим ID
+    [Fact]
+    [Trait("Category", "Error")]
+    public async Task GetEventByNonExistentId()
+    {
+        await ResetDatabaseAsync();
+
+        // Arrange
+        var eventId = Guid.NewGuid();
+
+        // Act
+        var repositoryForCheck = new EventRepository(CreateContext());
+        Func<Task> act = async () => await repositoryForCheck.SelectEventByIdAsync(eventId);
+
+        // Assert
+        await act.Should().ThrowAsync<EventNotFoundException>()
+           .WithMessage($"Can't get event with id = {eventId}. It is absent");
+    }
+
+    // Попытка обновить событие с несуществующим ID
+    [Fact]
+    [Trait("Category", "Success")]
+    public async Task GetUpdateEventWithNonExistentId()
+    {
+        await ResetDatabaseAsync();
+
+        // Arrange
+        var eventId = Guid.NewGuid();
+        var eventForUpdate = new Event(id: eventId,
+                title: "event 1 [updated]",
+                description: "Description of event 1 [updated]",
+                totalSeats: 1,
+                startAt: new DateTime(2026, 05, 01, 0, 0, 0, DateTimeKind.Utc),
+                endAt: new DateTime(2026, 05, 03, 0, 0, 0, DateTimeKind.Utc));
+
+        // Act
+        var repositoryForCheck = new EventRepository(CreateContext());
+        Func<Task> act = async () => await repositoryForCheck.UpdateEventAsync(eventForUpdate);
+
+        // Assert
+        await act.Should().ThrowAsync<EventNotFoundException>()
+           .WithMessage($"Can't update event with id = {eventId}. It is absent");
+    }
+}
