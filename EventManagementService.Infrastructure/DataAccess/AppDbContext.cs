b9233f5ebca87bf0a19cea047a78df54afa48da5
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-01 17:53:48 +0500

    [add] infrastructure

---
+++ EventManagementService.Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using EventManagementService.Domain.Models;
+using EventManagementService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagementService.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
+        : base(options) { }
+
+    public DbSet<Event> Events => Set<Event>();
+    public DbSet<Booking> Bookings => Set<Booking>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
---
+++ EventManagementService.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,111 @@
+﻿using EventManagementService.Application.Repositories;
+using EventManagementService.DataAccess;
+using EventManagementService.Domain.Exceptions;
+using EventManagementService.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManagementService.Infrastructure.Repositories;
+
+public class BookingRepository : IBookingRepository
+{
+    private readonly AppDbContext _dbc;
+
+    private static readonly SemaphoreSlim _bookingSemaphore = new(1, 1);
+
+    public BookingRepository(AppDbContext dbc)
+    {
+        _dbc = dbc;
+    }
+
+    public async Task<Booking> InsertBookingAsync(Booking newBooking, CancellationToken ct = default)
+    {
+        try
+        {
+            await _bookingSemaphore.WaitAsync(ct);
+
+            await _dbc.Bookings.AddAsync(newBooking, ct);
+
+            await _dbc.SaveChangesAsync(ct);
+
+            return newBooking;
+        }
+        finally
+        {
+            _bookingSemaphore.Release();
+        }
+    }
+
+    public async Task<Booking> SelectBookingByIdAsync(Guid bookingId, CancellationToken ct = default)
+    {
+        try
+        {
+            await _bookingSemaphore.WaitAsync(ct);
+
+            var result = await _dbc.Bookings
+                .Where(x => x.Id == bookingId)
+                .FirstOrDefaultAsync(ct);
+
+            if (result != null)
+            {
+                return result;
+            }
+            else
+            {
+                throw new BookingNotFoundException(bookingId,
+                    $"Can't get booking with id = {bookingId}. It is absent");
+            }
+        }
+        finally
+        {
+            _bookingSemaphore.Release();
+        }
+    }
+
+    public async Task<IReadOnlyList<Booking>> SelectAllBookingByStatusAsync(BookingStatus status, CancellationToken ct = default)
+    {
+        try
+        {
+            await _bookingSemaphore.WaitAsync(ct);
+
+            var result = await _dbc.Bookings
+                .Where(x => x.Status == status)
+                .ToListAsync(ct);
+
+            return result;
+        }
+        finally
+        {
+            _bookingSemaphore.Release();
+        }
+    }
+
+    public async Task UpdateBookingAsync(Guid id, Booking newBooking, CancellationToken ct = default)
+    {
+        try
+        {
+            await _bookingSemaphore.WaitAsync(ct);
+
+            var bookingForUpdate = await _dbc.Bookings
+                .Where(x => x.Id == id)
+                .FirstOrDefaultAsync(ct);
+
+            if (bookingForUpdate is null)
+            {
+                throw new BookingNotFoundException(id,
+                   $"Can't get booking with id = {id}. It is absent");
+            }
+
+            bookingForUpdate.EventId = newBooking.EventId;
+            bookingForUpdate.Status = newBooking.Status;
+            bookingForUpdate.CreatedAt = newBooking.CreatedAt;
+            bookingForUpdate.ProcessedAt = newBooking.ProcessedAt;
+
+
+            await _dbc.SaveChangesAsync(ct);
+        }
+        finally
+        {
+            _bookingSemaphore.Release();
+        }
+    }
+}
