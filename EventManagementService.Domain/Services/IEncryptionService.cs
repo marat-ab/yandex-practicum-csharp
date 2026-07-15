9dff7a235d0f4bc24c206dda8fb114376d8779bf
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-15 14:47:37 +0500

    [add] service for hash calc

---
+++ EventManagementService.Domain/Services/IEncryptionService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.Domain.Services;
+
+public interface IEncryptionService
+{
+    string CalcHash(string data);
+}
