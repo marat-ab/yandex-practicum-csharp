5890a13e48df255828cf304e15e112d6f50e3af3
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-15 18:00:04 +0500

    [upd] booking and event controllers

--- EventManagementService/Controllers/BookingsController.cs
+++ EventManagementService/Controllers/BookingsController.cs
@@ -1,10 +1,13 @@
 ﻿using EventManagementService.Application.Models.Dto;
 using EventManagementService.Application.Models.Extensions;
 using EventManagementService.Application.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace EventManagementService.Presentation.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("/[controller]")]
 public class BookingsController : ControllerBase
@@ -19,10 +22,42 @@ public class BookingsController : ControllerBase
     [HttpGet("{id:Guid}")]
     public async Task<ActionResult<BookingResponseDto>> GetBookingById(Guid id)
     {
-        var bookingItem = await _bookingService.GetBookingByIdAsync(id);
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
 
-        var result = bookingItem.ToBookingResponseDto();
+        if (userIdClaim == null)
+            return BadRequest("User id not found");
 
-        return Ok(result);
+        if (Guid.TryParse(userIdClaim.Value, out Guid userId))
+        {
+            var bookingItem = await _bookingService.GetBookingByIdAsync(id, userId);
+
+            var result = bookingItem.ToBookingResponseDto();
+
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest("Bad user id");
+        }
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<ActionResult> DeleteBookingById(Guid id)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            return BadRequest("User id not found");
+
+        if (Guid.TryParse(userIdClaim.Value, out Guid userId))
+        {
+            await _bookingService.CancelBookingAsync(id, userId);
+
+            return NoContent();
+        }
+        else
+        {
+            return BadRequest("Bad user id");
+        }
     }
 }
--- EventManagementService/Middlewares/CustomExceptionHandlingMiddleware.cs
+++ EventManagementService/Middlewares/CustomExceptionHandlingMiddleware.cs
@@ -65,7 +65,7 @@ public class CustomExceptionHandlingMiddleware
             EventNotFoundException _ => StatusCodes.Status404NotFound,
             EventAlreadyStartedException _ => StatusCodes.Status400BadRequest,
             BookingNotFoundException _ => StatusCodes.Status404NotFound,
-            BookingUserOverflowException _ => StatusCodes.Status404NotFound,
+            BookingUserOverflowException _ => StatusCodes.Status409Conflict,
             BookingCancelAccessDeniedException _ => StatusCodes.Status403Forbidden,
             NoAvailableSeatsException _ => StatusCodes.Status409Conflict,
             _ => StatusCodes.Status500InternalServerError
