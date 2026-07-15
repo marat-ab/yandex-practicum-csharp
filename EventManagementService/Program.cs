4848761c70591d800a4d6073f73a942903efcdea
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-15 18:01:50 +0500

    [add] auth to swagger

--- EventManagementService/Program.cs
+++ EventManagementService/Program.cs
@@ -2,12 +2,17 @@
 using EventManagementService.Application.HostedServices;
 using EventManagementService.Application.Repositories;
 using EventManagementService.Application.Services;
+using EventManagementService.Domain;
 using EventManagementService.Infrastructure;
 using EventManagementService.Infrastructure.DataAccess;
 using EventManagementService.Infrastructure.Models;
 using EventManagementService.Infrastructure.Repositories;
 using EventManagementService.Presentation.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +21,7 @@ builder.Services.AddControllers();
 
 builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddDomain();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
@@ -29,7 +35,54 @@ builder.Services.Configure<JWTSettings>(
     builder.Configuration.GetSection("JWT")
 );
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Event Management API",
+        Version = "v1"
+    });
+
+    options.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            Description = "Введите JWT-токен без префикса Bearer."
+        });
+
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetSection("JWT").GetValue<string>("Issuer"),
+
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("JWT").GetValue<string>("Audience"),
+
+        ValidateIssuerSigningKey = true,
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT").GetValue<string>("Key"))),
+
+        ValidateLifetime = true
+    };
+});
 
 var app = builder.Build();
 
@@ -45,11 +98,17 @@ app.UseMiddleware<CustomExceptionHandlingMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Event Management API v1");
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
