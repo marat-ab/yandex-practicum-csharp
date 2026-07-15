b2a220e910e8a84f6b2b896bcfa11dde3febd30f
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-15 16:32:33 +0500

    [add] migration with add users table

--- EventManagementService/Program.cs
+++ EventManagementService/Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddHttpContextAccessor();
+
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
 
