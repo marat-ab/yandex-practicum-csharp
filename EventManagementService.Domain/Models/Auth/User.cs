a5affb7875533afb57f6b0282e0a716ab949295d
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-15 14:03:56 +0500

    [add] user model

---
+++ EventManagementService.Domain/Models/Auth/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.Domain.Models.Auth;
+
+public class User
+{
+    public User(long id, string login, string passHash, Role role)
+    {
+        Id = id;
+        Login = login;
+        PasswordHash = passHash;
+        Role = role;
+    }
+
+    public long Id { get; set; }
+    public string Login { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public Role Role { get; set; }
+}
