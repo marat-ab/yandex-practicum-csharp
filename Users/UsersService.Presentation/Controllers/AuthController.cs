02950bc5c13c02f3e246e848a8a312aa9303a506
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 17:52:40 +0500

    [add] users service presentation

---
+++ Users/UsersService.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsersService.Application.Models.Dto;
+using UsersService.Application.Services;
+using UsersService.Domain.Models.Auth;
+
+namespace UsersService.Presentation.Controllers;
+
+[ApiController]
+[Route("/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AuthController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult> Register([FromBody] UserRegisterRequestDto userRequest)
+    {
+        var role = userRequest.Role is null
+            ? Role.User
+            : userRequest.Role.Value;
+
+        var _ = await _userService.CreateUserAsync(userRequest.Login, userRequest.Password, role);
+
+        return NoContent();
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<string>> Login([FromBody] UserLoginRequestDto userRequest)
+    {
+        var accessToken = await _userService.LoginAsync(userRequest.Login, userRequest.Password);
+
+        return Ok(new { Token = accessToken });
+    }
+}
