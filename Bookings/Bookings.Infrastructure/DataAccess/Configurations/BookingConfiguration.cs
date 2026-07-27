4c18a343e542ff698e6a9ef80ce95c222779422a
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 18:25:41 +0500

    [add] booking service infrastructure

---
+++ Bookings/Bookings.Infrastructure/DataAccess/Configurations/BookingConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Bookings.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookings.Infrastructure.DataAccess.Configurations;
+
+public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+{
+    public void Configure(EntityTypeBuilder<Booking> builder)
+    {
+        builder.ToTable("bookings");
+
+        builder.HasKey(b => b.Id);
+        builder.Property(x => x.Id)
+           .ValueGeneratedNever();
+
+        builder.Property(b => b.EventId)
+           .IsRequired();
+
+        builder.Property(b => b.Status)
+           .IsRequired()
+           .HasConversion<string>();
+
+        builder.Property(b => b.CreatedAt)
+           .IsRequired();
+
+        builder.Property(b => b.ProcessedAt);
+    }
+}
---
+++ Bookings/Bookings.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Bookings.Infrastructure.Repositories;
+using EventManagementService.Application.HostedServices;
+using EventManagementService.Application.Repositories;
+using EventManagementService.Application.Services;
+using EventManagementService.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventManagementService.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddScoped<IEventRepository, EventRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        services.AddTransient<IUserRepository, UserRepository>();
+
+        services.AddTransient<IJWTService, JWTService>();
+
+        return services;
+    }
+}
