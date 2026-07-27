6638e88edf9a0e04fd17148f4d14f595025ad973
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 17:52:10 +0500

    [add] users service application

---
+++ Users/UsersService.Application/Models/Dto/UserLoginRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersService.Application.Models.Dto;
+
+public class UserLoginRequestDto
+{
+    [Required(ErrorMessage = "Логин (Login) обязателен для заполнения")]
+    public string Login { get; init; } = string.Empty;
+
+    [Required(ErrorMessage = "Пароль (Password) обязателен для заполнения")]
+    public string Password { get; init; } = string.Empty;
+}
