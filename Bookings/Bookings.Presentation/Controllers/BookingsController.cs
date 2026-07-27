e9ee4a08ab356f8dadd24e77bff15259058d3e5b
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 18:25:54 +0500

    [add] bookings service presentation

---
+++ Bookings/Bookings.Presentation/Controllers/BookingsController.cs
@@ -0,0 +1,69 @@
+﻿using Bookings.Application.Models.Dto;
+using Bookings.Application.Services;
+using Bookings.Domain.Models.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Bookings.Application.Models.Extensions;
+
+namespace Bookings.Presentation.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("/[controller]")]
+public class BookingsController : ControllerBase
+{
+    private readonly IBookingService _bookingService;
+
+    public BookingsController(IBookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
+    [HttpGet("{id:Guid}")]
+    public async Task<ActionResult<BookingResponseDto>> GetBookingById(Guid id)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+
+        if (userIdClaim == null)
+            return BadRequest("User id not found");
+
+        if (Guid.TryParse(userIdClaim.Value, out Guid userId))
+        {
+            var bookingItem = await _bookingService.GetBookingByIdAsync(id, userId);
+
+            var result = bookingItem.ToBookingResponseDto();
+
+            return Ok(result);
+        }
+        else
+        {
+            return BadRequest("Bad user id");
+        }
+    }
+
+    [HttpDelete("{id:Guid}")]
+    public async Task<ActionResult> DeleteBookingById(Guid id)
+    {
+        var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+        var userRoleClaim = User.FindFirst(ClaimTypes.Role);
+
+        if (userIdClaim == null)
+            return BadRequest("User id not found");
+
+        if (userRoleClaim == null)
+            return BadRequest("Role not found");
+
+        if (Guid.TryParse(userIdClaim.Value, out Guid userId)
+            && Enum.TryParse(userRoleClaim.Value, out Role role))
+        {
+            await _bookingService.CancelBookingAsync(id, userId, role);
+
+            return NoContent();
+        }
+        else
+        {
+            return BadRequest("Bad user id or role");
+        }
+    }
+}
---
+++ Bookings/Bookings.Presentation/Program.cs
@@ -0,0 +1,110 @@
+﻿using Bookings.Application;
+using Bookings.Domain;
+using Bookings.Domain.Models;
+using Bookings.Infrastructure.DataAccess;
+using Bookings.Presentation.Middlewares;
+using EventManagementService.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+builder.Services.AddDomain();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+
+var connectionString = builder.Configuration.GetConnectionString("Default")
+    ?? throw new InvalidOperationException("Connection string 'Default' not found.");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.Configure<SystemSettings>(
+    builder.Configuration.GetSection("SystemSettings")
+);
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Event Management API",
+        Version = "v1"
+    });
+
+    options.AddSecurityDefinition(
+        "Bearer",
+        new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            Description = "Введите JWT-токен без префикса Bearer."
+        });
+
+    options.AddSecurityRequirement(document =>
+        new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+        });
+});
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = builder.Configuration.GetSection("JWT").GetValue<string>("Issuer"),
+
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration.GetSection("JWT").GetValue<string>("Audience"),
+
+        ValidateIssuerSigningKey = true,
+
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration.GetSection("JWT").GetValue<string>("Key"))),
+
+        ValidateLifetime = true
+    };
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+app.UseMiddleware<CustomExceptionHandlingMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Event Management API v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
