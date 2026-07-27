94646263b40189b30a11bd0138ffc8b1460da8a8
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 18:25:30 +0500

    [add] booking service application

---
+++ Bookings/Bookings.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Bookings.Application.Services;
+using EventManagementService.Application.HostedServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookings.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IBookingService, BookingService>();
+
+        services.AddHostedService<BookingHostedService>();
+
+        return services;
+    }
+}
---
+++ Bookings/Bookings.Application/Services/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using Bookings.Domain.Models;
+using Bookings.Domain.Models.Auth;
+
+namespace Bookings.Application.Services;
+
+public interface IBookingService
+{
+    Task<Booking> CreateBookingAsync(Guid eventId, Guid userId, CancellationToken ct = default);
+
+    Task CancelBookingAsync(Guid bookingId, Guid userId, Role role, CancellationToken ct = default);
+
+    Task<Booking> GetBookingByIdAsync(Guid bookingId, Guid userId, CancellationToken ct = default);
+    Task<IReadOnlyList<Booking>> GetAllBookingByStatusAsync(BookingStatus status, CancellationToken ct = default);
+
+    Task UpdateBookingAsync(Guid bookingId, Booking newBooking, CancellationToken ct = default);
+}
