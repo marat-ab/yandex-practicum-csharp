0a253d45344784fa5ab96f817077f22721c46dcf
Marat Abdrakhmanov <abdrakhmanovmi@yandex.ru>
2026-07-27 17:51:53 +0500

    [add] users service domain

---
+++ Users/UsersService.Domain/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using UsersService.Domain.Services;
+
+namespace UsersService.Domain;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services)
+    {
+        services.AddTransient<IEncryptionService, EncryptionService>();
+
+        return services;
+    }
+}
