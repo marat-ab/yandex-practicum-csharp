12e6e869323b77dd600ba47b5c297ed4d5b1d7ab
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-07-29 23:09:13 +0500

    [add] broker library

---
+++ BrokerLibrary/Kafka/BookingConfirmed.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrokerLibrary.Kafka;
+
+public record BookingConfirmed(Guid EventId);
---
+++ BrokerLibrary/Kafka/KafkaTopics.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrokerLibrary.Kafka;
+
+public static class KafkaTopics
+{
+    public const string BookingConfirmed = "booking-confirmed";
+}
