dbe7f418479f054f9d5506790b3cde22412c4e36
Marat Abdrakhmanov <marat-ab@mail.ru>
2026-08-08 18:35:13 +0500

    [add] StackExchange.Redis and connection to redis to DI as singleton

---
+++ Events/EventsService.Domain/Models/RedisSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsService.Domain.Models;
+
+public class RedisSettings
+{
+    public string RedisServer { get; set; } = string.Empty;
+}
